9b55a06a3011af86851eb5e7dc55040049f059ca
Charlie Gray <charliechasegray@gmail.com>
2023-03-05 20:24:52 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
+
 */
 
 /*
   Add written explanation of your solution here
+
+  splits word into array of letters, then reverses them and rejoins them into a "new" word
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
